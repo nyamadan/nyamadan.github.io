986773cb20df4521dcff9572fd70c0cd0987ed42
Yoshiki YAMADA <nyamadandan@gmail.com>
2021-02-21 16:29:35 +0900

    fix gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,11 +16,11 @@ gtag('config', 'UA-17058943-8');
 `;
 
 const gtm = `
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NV8B2M9');</script>
+})(window,document,'script','dataLayer','GTM-NV8B2M9');
 <!-- End Google Tag Manager -->
 `;
 
