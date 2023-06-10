f9ffeb05daceb11570b73a22dc8cc78b24c24dcc
Yoshiki YAMADA <nyamadandan@gmail.com>
2023-06-10 18:46:09 +0900

    GAを削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,22 +3,6 @@ import Document from "next/dist/pages/_document";
 import { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-const gtag = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-17058943-8');
-`;
-
-const gtm = `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NV8B2M9');
-<!-- End Google Tag Manager -->
-`;
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
@@ -50,17 +34,6 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <meta charSet="utf-8" />
-          <script
-            src="https://www.googletagmanager.com/gtag/js?id=UA-17058943-8"
-            async
-          />
-          {/* eslint-disable-next-line react/no-danger */}
-          <script dangerouslySetInnerHTML={{ __html: gtag }} />
-          {/* <!-- Google Tag Manager --> */}
-          {/* eslint-disable-next-line react/no-danger */}
-          <script dangerouslySetInnerHTML={{ __html: gtm }} />
           <link
             rel="alternate"
             type="application/rss+xml"
@@ -70,16 +43,6 @@ class MyDocument extends Document {
           <link key="favicon" rel="icon" href="/favicon.ico" />
         </Head>
         <body>
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NV8B2M9"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
